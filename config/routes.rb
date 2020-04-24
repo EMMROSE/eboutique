d37f45e1e506daa7fdbb86df25d5a197e39ac1f2
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-04-24 16:51:32 +0200

    payment

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :products
-  resources :orders
+  resources :orders do
+    resources :payments, only: :new
+  end
   resources :line_items
     post 'line_items/:id/add', to: "line_items#add_quantity", as: "line_item_add"
     post 'line_items/:id/reduce', to: "line_items#reduce_quantity", as: "line_item_reduce"
   resources :carts
+  post 'without_login', to: 'orders#without_login'
   # resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
