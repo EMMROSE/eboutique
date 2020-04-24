c799a74d904f1fa77190f9010d83b25789ab6040
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-04-24 14:54:06 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,25 @@ puts 'start seeds...'
 
 Product.destroy_all
 product1 = Product.create({:name=>"tomato", :description=>"super tomato", :sku=>"123456", price_cents: 320})
-product1.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
 product2 = Product.create({:name=>"milk", :description=>"super milk", :sku=>"123457",price_cents: 320})
-product2.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
-product3 = Product.create({:name=>"bread", :description=>"super bread", :sku=>"123458",price_cents: 320})
-product3.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product3 = Product.create({:name=>"bread", :description=>"super bread", :sku=>"123458",price_cents: 350})
 product4 = Product.create({:name=>"bacon", :description=>"super bacon", :sku=>"123459",price_cents: 320})
-product4.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
-product5 = Product.create({:name=>"cheese", :description=>"super cheese", :sku=>"123450",price_cents: 320})
-product5.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product5 = Product.create({:name=>"cheese", :description=>"super cheese", :sku=>"123450",price_cents: 420})
 puts "Total number of products: #{Product.all.count}"
+file = URI.open('https://res.cloudinary.com/dwrzyhvzy/image/upload/v1585089570/atelier/verriere.jpg')
+product1.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts 'number 1 finished'
+file = URI.open('https://res.cloudinary.com/dwrzyhvzy/image/upload/v1585089570/atelier/verriere.jpg')
+product2.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts 'number 2 finished'
+file = URI.open('https://res.cloudinary.com/dwrzyhvzy/image/upload/v1585089570/atelier/verriere.jpg')
+product3.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts 'number 3 finished'
+file = URI.open('https://res.cloudinary.com/dwrzyhvzy/image/upload/v1585089570/atelier/verriere.jpg')
+product4.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts 'number 4 finished'
+file = URI.open('https://res.cloudinary.com/dwrzyhvzy/image/upload/v1585089570/atelier/verriere.jpg')
+product5.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts 'number 5 finished'
+puts '...'
 puts 'seeds finished'
