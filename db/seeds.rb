68514590a136fa6948735079d9270b14ec4f2ad0
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-04-24 11:06:05 +0200

    asset pciture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'start seeds...'
+
+Product.destroy_all
+product1 = Product.create({:name=>"tomato", :description=>" super tomato", :sku=>"123456",:price => 1})
+product1.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product2 = Product.create({:name=>"milk", :description=>" super milk", :sku=>"123457",:price => 3})
+product2.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product3 = Product.create({:name=>"bread", :description=>" super bread", :sku=>"123458",:price => 550})
+product3.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product4 = Product.create({:name=>"bacon", :description=>" super bacon", :sku=>"123459",:price => 10})
+product4.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+product5 = Product.create({:name=>"cheese", :description=>" super cheese", :sku=>"123450",:price => 320})
+product5.photo.attach(io: file, filename: 'verriere.jpg', content_type: 'image/jpg')
+puts "Total number of products: #{Product.all.count}"
+puts 'seeds finished'
