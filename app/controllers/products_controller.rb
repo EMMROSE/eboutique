9f67dad00dc06b3b0c025bd8dd3072575463583b
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-05-05 11:38:41 +0200

    controller for events

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @products = Product.all
@@ -9,4 +9,43 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash.notice = "Thank you! you add successfully a new product."
+      redirect_to root_path
+    else
+      #flash.now[:error] = t('flash.work.error_html')
+      render :new
+    end
+  end
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :sku, :photo, :cover, :price_cents)
+  end
 end
+
