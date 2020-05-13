7eda23582b548a71b3b7752accdc52de1ac2b65e
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-05-13 18:56:49 +0200

    orders

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(state: 'paid')
+    order.update(status: 'payé')
     # Send an email to confirmate
     OrderMailer.confirmation(@order).deliver_now
   end
