3c4c2e52e0bda1b70f2ff3e1852221452f0682b1
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-06-06 08:57:47 +0200

    delivery

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,10 +6,28 @@ class CartsController < ApplicationController
       @line_items = @cart.line_items.order(:created_at)
     end
 
+    def edit
+      @cart = Cart.find(params[:id])
+    end
+
+    def update
+      @cart = Cart.find(params[:id])
+      if @cart.update(cart_params)
+        redirect_to new_order_path
+      else render :edit
+      end
+    end
+
     def destroy
       @cart = @current_cart
       @cart.destroy
       session[:cart_id] = nil
       redirect_to root_path
+    end
+
+    private
+
+    def cart_params
+      params.require(:cart).permit(:delivery)
     end
 end
